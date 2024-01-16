fdd0400c536e427b258127e580785900b9ba34e5
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-16 18:49:05 +0300

    Disable redirect on unauthorized request

--- src/Auth.Simple/Program.cs
+++ src/Auth.Simple/Program.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,7 +17,17 @@ services.AddSwaggerGen();
 
 services
     .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie();
+    .AddCookie(options =>
+    {
+        options.Cookie.HttpOnly = true;
+        options.LoginPath = string.Empty;
+        options.AccessDeniedPath = string.Empty;
+        options.Events.OnRedirectToLogin = context =>
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            return Task.CompletedTask;
+        };
+    });
 
 services.AddRouting(options => options.LowercaseUrls = true);
 
