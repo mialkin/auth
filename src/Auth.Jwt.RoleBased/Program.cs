05419a121f8faedfb3a520dcbe2d36859e51ced9
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-18 15:03:40 +0300

    Add Swagger authorization header

--- src/Auth.Jwt.RoleBased/Program.cs
+++ src/Auth.Jwt.RoleBased/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using Auth.Jwt.RoleBased;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,7 +17,39 @@ var services = builder.Services;
 
 services.AddControllers();
 services.AddRouting(options => options.LowercaseUrls = true);
-services.AddSwaggerGen(options => { options.DescribeAllParametersInCamelCase(); });
+services.AddSwaggerGen(options =>
+{
+    options.DescribeAllParametersInCamelCase();
+
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Auth JWT role-based", Version = "v1" });
+
+    // This line adds Authorize button in the top right corner of the screen
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme"
+    });
+
+    // This line adds "-H 'Authorization: Bearer YOUR_JWT_TOKEN" if YOUR_JWT_TOKEN is specified
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var settings = builder.Configuration.GetRequiredSection(nameof(JwtTokenSettings)).Get<JwtTokenSettings>();
 if (settings is null)
