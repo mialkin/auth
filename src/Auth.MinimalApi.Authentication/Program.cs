c3d2f6dccf9f46ad969f0db0569ba2b821e4d1b2
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-19 02:10:06 +0300

    Add IDataProtectionProvider

--- src/Auth.MinimalApi.Authentication/Program.cs
+++ src/Auth.MinimalApi.Authentication/Program.cs
@@ -1,22 +1,38 @@
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/username", (HttpContext context) =>
+builder.Services.AddDataProtection(); // Adds IDataProtectionProvider in IoC container
+
+var application = builder.Build();
+
+application.MapGet("/username", (HttpContext context, IDataProtectionProvider provider) =>
 {
+    var protector = provider.CreateProtector("auth-cookie");
+    
     var authCookie = context.Request.Headers.Cookie.FirstOrDefault(x => x != null && x.StartsWith("auth="));
-    var payload = authCookie?.Split("=").Last();
-    var parts = payload?.Split(":");
+    var protectedPayload = authCookie?.Split("=").Last();
 
-    var key = parts?[0];
-    var value = parts?[1];
+    if (protectedPayload != null)
+    {
+        var payload = protector.Unprotect(protectedPayload);
+        var parts = payload?.Split(":");
 
-    return value ?? "N/A";
+        var key = parts?[0];
+        var value = parts?[1];
+
+        return value ?? string.Empty;
+    }
+
+    return "N/A";
 });
 
-app.MapGet("/login", (HttpContext context) =>
+application.MapGet("/login", (HttpContext context, IDataProtectionProvider provider) =>
 {
-    context.Response.Headers["set-cookie"] = "auth=usr:aleksei";
+    var protector = provider.CreateProtector("auth-cookie");
+    
+    context.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:aleksei")}";
     return "ok";
 });
 
-app.Run();
+application.Run();
