316f59a9a0201039f02a494403de998ff77f607c
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-19 01:56:05 +0300

    Implement /login and /username actions

--- src/Auth.MinimalApi.Authentication/Program.cs
+++ src/Auth.MinimalApi.Authentication/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/username", (HttpContext context) =>
+{
+    var authCookie = context.Request.Headers.Cookie.FirstOrDefault(x => x != null && x.StartsWith("auth="));
+    var payload = authCookie?.Split("=").Last();
+    var parts = payload?.Split(":");
+
+    var key = parts?[0];
+    var value = parts?[1];
+
+    return value;
+});
+
+app.MapGet("/login", (HttpContext context) =>
+{
+    context.Response.Headers["set-cookie"] = "auth=usr:aleksei";
+    return "ok";
+});
 
 app.Run();
