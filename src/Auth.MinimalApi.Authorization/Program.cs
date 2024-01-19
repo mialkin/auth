5b6b8179220385aec087bd4e23fb9bed3efba56f
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-19 15:53:35 +0300

    Set up bath authentication

--- src/Auth.MinimalApi.Authorization/Program.cs
+++ src/Auth.MinimalApi.Authorization/Program.cs
@@ -1,6 +1,27 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+const string authScheme = "cookie";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(authScheme)
+    .AddCookie(authScheme);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();
+
+app.MapGet("/unsecure", (HttpContext ctx) => { return ctx.User.FindFirst("usr")?.Value ?? "empty"; });
+
+app.MapGet("/login", async (HttpContext context) =>
+{
+    var claims = new List<Claim>();
+    claims.Add(new Claim("usr", "aleksei"));
+    var identity = new ClaimsIdentity(claims, authScheme);
+    var user = new ClaimsPrincipal(identity);
+
+    await context.SignInAsync(authScheme, user);
+});
 
 app.Run();
