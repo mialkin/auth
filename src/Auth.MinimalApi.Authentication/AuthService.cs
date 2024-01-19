de157f0eff7a74c5f54b7f8b2556186f44393fd9
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-19 14:22:48 +0300

    Use built-in authentication

--- src/Auth.MinimalApi.Authentication/AuthService.cs
+++ src/Auth.MinimalApi.Authentication/AuthService.cs
@@ -2,22 +2,22 @@ using Microsoft.AspNetCore.DataProtection;
 
 namespace Auth.MinimalApi.Authentication;
 
-public class AuthService
-{
-    private readonly IDataProtectionProvider _provider;
-    private readonly IHttpContextAccessor _accessor;
-
-    public AuthService(IDataProtectionProvider provider, IHttpContextAccessor accessor)
-    {
-        _provider = provider;
-        _accessor = accessor;
-    }
-
-    public void SignIn()
-    {
-        var protector = _provider.CreateProtector("auth-cookie");
-
-        if (_accessor.HttpContext != null)
-            _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:aleksei")}";
-    }
-}
+// public class AuthService
+// {
+//     private readonly IDataProtectionProvider _provider;
+//     private readonly IHttpContextAccessor _accessor;
+//
+//     public AuthService(IDataProtectionProvider provider, IHttpContextAccessor accessor)
+//     {
+//         _provider = provider;
+//         _accessor = accessor;
+//     }
+//
+//     public void SignIn()
+//     {
+//         var protector = _provider.CreateProtector("auth-cookie");
+//
+//         if (_accessor.HttpContext != null)
+//             _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:aleksei")}";
+//     }
+// }
--- src/Auth.MinimalApi.Authentication/Program.cs
+++ src/Auth.MinimalApi.Authentication/Program.cs
@@ -1,52 +1,59 @@
 using System.Security.Claims;
-using Auth.MinimalApi.Authentication;
-using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDataProtection(); // Adds IDataProtectionProvider to IoC container
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<AuthService>();
+// builder.Services.AddDataProtection(); // Adds IDataProtectionProvider to IoC container
+// builder.Services.AddHttpContextAccessor();
+// builder.Services.AddScoped<AuthService>();
+builder.Services.AddAuthentication("cookie")
+    .AddCookie("cookie");
 
 var application = builder.Build();
+application.UseAuthentication();
 
-application.Use((context, next) =>
-{
-    var provider = context.RequestServices.GetRequiredService<IDataProtectionProvider>();
-
-    var protector = provider.CreateProtector("auth-cookie");
-
-    var authCookie = context.Request.Headers.Cookie.FirstOrDefault(x => x != null && x.StartsWith("auth="));
-    var protectedPayload = authCookie?.Split("=").Last();
-
-    if (protectedPayload != null)
-    {
-        var payload = protector.Unprotect(protectedPayload);
-        var parts = payload.Split(":");
-
-        var key = parts[0];
-        var value = parts[1];
-
-        var claims = new List<Claim>
-        {
-            new(key, value)
-        };
-
-        var identity = new ClaimsIdentity(claims);
-        context.User = new ClaimsPrincipal(identity);
-    }
-
-    return next();
-});
+// application.Use((context, next) =>
+// {
+//     var provider = context.RequestServices.GetRequiredService<IDataProtectionProvider>();
+//
+//     var protector = provider.CreateProtector("auth-cookie");
+//
+//     var authCookie = context.Request.Headers.Cookie.FirstOrDefault(x => x != null && x.StartsWith("auth="));
+//     var protectedPayload = authCookie?.Split("=").Last();
+//
+//     if (protectedPayload != null)
+//     {
+//         var payload = protector.Unprotect(protectedPayload);
+//         var parts = payload.Split(":");
+//
+//         var key = parts[0];
+//         var value = parts[1];
+//
+//         var claims = new List<Claim>
+//         {
+//             new(key, value)
+//         };
+//
+//         var identity = new ClaimsIdentity(claims);
+//         context.User = new ClaimsPrincipal(identity);
+//     }
+//
+//     return next();
+// });
 
 application.MapGet("/username", (HttpContext context) =>
 {
     return context.User.FindFirst("usr")?.Value ?? "N/A";
 });
 
-application.MapGet("/login", (AuthService authService) =>
+application.MapGet("/login", async (HttpContext context) =>
 {
-    authService.SignIn();
+    var claims = new List<Claim>();
+    claims.Add(new Claim("usr", "aleksei"));
+    var identity = new ClaimsIdentity(claims, "cookie");
+    var user = new ClaimsPrincipal(identity);
+    
+    await context.SignInAsync("cookie", user);
 
     return "ok";
 });
