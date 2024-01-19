2b4a9079e2fda84629e156aecc470db7e89b3cd2
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-19 17:40:02 +0300

    Add /norway and /sweden endpoints and "cookie2" scheme

--- src/Auth.MinimalApi.Authorization/Program.cs
+++ src/Auth.MinimalApi.Authorization/Program.cs
@@ -2,11 +2,13 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 
 const string authScheme = "cookie";
+const string authScheme2 = "cookie2";
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthentication(authScheme)
-    .AddCookie(authScheme);
+    .AddCookie(authScheme)
+    .AddCookie(authScheme2);
 
 var app = builder.Build();
 
@@ -17,6 +19,40 @@ app.MapGet("/unsecure", (HttpContext ctx) => { return ctx.User.FindFirst("usr")?
 app.MapGet("/sweden", (HttpContext context) =>
 {
     if (context.User.Identities.All(x => x.AuthenticationType != authScheme))
+    {
+        context.Response.StatusCode = 401;
+        return "";
+    }
+
+    if (!context.User.HasClaim("passport_type", "eur"))
+    {
+        context.Response.StatusCode = 403;
+        return "";
+    }
+
+    return "allowed";
+});
+
+app.MapGet("/norway", (HttpContext context) =>
+{
+    if (context.User.Identities.All(x => x.AuthenticationType != authScheme))
+    {
+        context.Response.StatusCode = 401;
+        return "";
+    }
+
+    if (!context.User.HasClaim("passport_type", "NOR"))
+    {
+        context.Response.StatusCode = 403;
+        return "";
+    }
+
+    return "allowed";
+});
+
+app.MapGet("/denmark", (HttpContext context) =>
+{
+    if (context.User.Identities.All(x => x.AuthenticationType != authScheme2))
     {
         context.Response.StatusCode = 401;
         return "";
