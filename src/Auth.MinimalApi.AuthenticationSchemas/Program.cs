d8a990af1b51103dec6825be9bf0e19f88737151
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-19 23:56:58 +0300

    Setup project

--- src/Auth.MinimalApi.AuthenticationSchemas/Program.cs
+++ src/Auth.MinimalApi.AuthenticationSchemas/Program.cs
@@ -1,6 +1,33 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var services = builder.Services;
 
-app.Run();
+services.AddAuthentication()
+    .AddCookie("local");
+
+services.AddAuthorization();
+
+var application = builder.Build();
+
+application.UseAuthentication();
+application.UseAuthorization();
+
+application.MapGet("/", () => "Hello world!");
+
+application.MapGet("/login", async (HttpContext context) =>
+{
+    var claims = new List<Claim>();
+    claims.Add(new Claim("usr", "aleksei"));
+    var identity = new ClaimsIdentity(claims, "cookie");
+    var user = new ClaimsPrincipal(identity);
+
+    await context.SignInAsync("local", user);
+
+    return "ok";
+});
+
+
+application.Run();
