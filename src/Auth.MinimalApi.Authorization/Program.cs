6815edb44f1f667cf1b587a91aa877768e34950b
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-19 17:31:22 +0300

    Add /sweden endpoint

--- src/Auth.MinimalApi.Authorization/Program.cs
+++ src/Auth.MinimalApi.Authorization/Program.cs
@@ -14,10 +14,28 @@ app.UseAuthentication();
 
 app.MapGet("/unsecure", (HttpContext ctx) => { return ctx.User.FindFirst("usr")?.Value ?? "empty"; });
 
+app.MapGet("/sweden", (HttpContext context) =>
+{
+    if (context.User.Identities.All(x => x.AuthenticationType != authScheme))
+    {
+        context.Response.StatusCode = 401;
+        return "";
+    }
+
+    if (!context.User.HasClaim("passport_type", "eur"))
+    {
+        context.Response.StatusCode = 403;
+        return "";
+    }
+
+    return "allowed";
+});
+
 app.MapGet("/login", async (HttpContext context) =>
 {
     var claims = new List<Claim>();
     claims.Add(new Claim("usr", "aleksei"));
+    claims.Add(new Claim("passport_type", "eur"));
     var identity = new ClaimsIdentity(claims, authScheme);
     var user = new ClaimsPrincipal(identity);
 
