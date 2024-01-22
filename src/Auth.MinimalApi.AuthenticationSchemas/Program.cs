0f9b0c9b8605dae9b5018e8fa06bad810e6e4fd9
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-22 22:57:27 +0300

    Add VisitorAuthHandler

--- src/Auth.MinimalApi.AuthenticationSchemas/Program.cs
+++ src/Auth.MinimalApi.AuthenticationSchemas/Program.cs
@@ -1,27 +1,44 @@
 using System.Security.Claims;
+using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
 services.AddAuthentication()
-    .AddCookie("local");
+    .AddScheme<CookieAuthenticationOptions, VisitorAuthHandler>("visitor", options => { })
+    .AddCookie("local"); // This is also another authentication schema
 
-services.AddAuthorization();
+services.AddAuthorization(options =>
+{
+    options.AddPolicy("customer", policy =>
+    {
+        policy.AddAuthenticationSchemes("local", "visitor")
+            .RequireAuthenticatedUser();
+    });
+});
 
 var application = builder.Build();
 
 application.UseAuthentication();
 application.UseAuthorization();
 
-application.MapGet("/", () => "Hello world!");
+application.MapGet("/", (HttpContext context) =>
+{
+    var user = context.User;
+    var identites = user.Identities;
 
-application.MapGet("/login", async (HttpContext context) =>
+    return "Hello world!";
+}).RequireAuthorization("customer");
+
+application.MapGet("/login-local", async (HttpContext context) =>
 {
     var claims = new List<Claim>();
     claims.Add(new Claim("usr", "aleksei"));
-    var identity = new ClaimsIdentity(claims, "cookie");
+    var identity = new ClaimsIdentity(claims, "local");
     var user = new ClaimsPrincipal(identity);
 
     await context.SignInAsync("local", user);
@@ -29,5 +46,32 @@ application.MapGet("/login", async (HttpContext context) =>
     return "ok";
 });
 
-
 application.Run();
+
+public class VisitorAuthHandler : CookieAuthenticationHandler
+{
+    public VisitorAuthHandler(
+        IOptionsMonitor<CookieAuthenticationOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder) : base(options, logger, encoder)
+    {
+    }
+
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var result = await base.HandleAuthenticateAsync();
+        if (result.Succeeded)
+        {
+            return result;
+        }
+
+        var claims = new List<Claim>();
+        claims.Add(new Claim("usr", "aleksei"));
+        var identity = new ClaimsIdentity(claims, "visitor");
+        var user = new ClaimsPrincipal(identity);
+
+        await Context.SignInAsync("visitor", user);
+
+        return AuthenticateResult.Success(new AuthenticationTicket(user, "visitor"));
+    }
+}
