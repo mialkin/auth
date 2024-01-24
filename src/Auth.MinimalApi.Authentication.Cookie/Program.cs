e2eb08c750a16e23337f3e2b9107375f4d4f0412
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-24 18:27:48 +0300

    Add sliding expiration

--- src/Auth.MinimalApi.Authentication.Cookie/Program.cs
+++ src/Auth.MinimalApi.Authentication.Cookie/Program.cs
@@ -5,13 +5,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
-services.AddAuthentication()
+services.AddAuthentication("default")
     .AddCookie("default", options =>
     {
         options.Cookie.Name = "mycookie";
         // options.Cookie.Domain = "mydomain";
-        options.Cookie.Path = "/test";
+        // options.Cookie.Path = "/test";
+        // options.Cookie.HttpOnly = false;
+        // options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        // options.Cookie.SameSite = SameSiteMode.Lax;
+
+        options.ExpireTimeSpan = TimeSpan.FromSeconds(10);
+        options.SlidingExpiration = true;
     });
+services.AddAuthorization();
 
 services.AddControllers();
 
@@ -19,22 +26,28 @@ var application = builder.Build();
 
 application.UseStaticFiles();
 application.UseAuthentication();
+application.UseAuthorization();
 
 application.MapGet("/", () => "Hello World!");
+application.MapGet("/test", () => "Hello World!").RequireAuthorization();
 application.MapPost("/login", async (HttpContext httpContext) =>
 {
-     await httpContext.SignInAsync(new ClaimsPrincipal(
-                new ClaimsIdentity(
-                    new[]
-                    {
-                        new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
-                    },
-                    authenticationType: "default"
-                )
+    await httpContext.SignInAsync(new ClaimsPrincipal(
+            new ClaimsIdentity(
+                new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                },
+                authenticationType: "default"
             )
-        );
+        ),
+        new AuthenticationProperties
+        {
+            IsPersistent = true
+        }
+    );
 
-        return "Ok";
+    return "Ok";
 });
 
 application.MapDefaultControllerRoute();
