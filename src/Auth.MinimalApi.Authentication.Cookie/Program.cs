82eaf6b308a963b990b7e0ddc5cd6968538d4997
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-24 13:26:00 +0300

    Add js.html page

--- src/Auth.MinimalApi.Authentication.Cookie/Program.cs
+++ src/Auth.MinimalApi.Authentication.Cookie/Program.cs
@@ -1,3 +1,6 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
@@ -16,6 +19,21 @@ application.UseStaticFiles();
 application.UseAuthentication();
 
 application.MapGet("/", () => "Hello World!");
+application.MapPost("/login", async (HttpContext httpContext) =>
+{
+     await httpContext.SignInAsync(new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                    },
+                    authenticationType: "default"
+                )
+            )
+        );
+
+        return "Ok";
+});
 
 application.MapDefaultControllerRoute();
 
