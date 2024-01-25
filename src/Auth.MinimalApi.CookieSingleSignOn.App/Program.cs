9b3e974d97a6900f3fd9baa840c2b349a81b64a1
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-25 06:23:58 +0300

    Add basic setup

--- src/Auth.MinimalApi.CookieSingleSignOn.App/Program.cs
+++ src/Auth.MinimalApi.CookieSingleSignOn.App/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello from the App!");
+var services = builder.Services;
 
-app.Run();
+services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+
+services.AddAuthorization();
+
+var application = builder.Build();
+
+application.UseAuthentication();
+application.UseAuthorization();
+
+application.MapGet("/", () => "Hello from the App!");
+application.MapGet("/protected", () => "Secret!").RequireAuthorization();
+
+application.Run();
--- src/Auth.MinimalApi.CookieSingleSignOn.Identity/Program.cs
+++ src/Auth.MinimalApi.CookieSingleSignOn.Identity/Program.cs
@@ -1,6 +1,36 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello from the Identity!");
+var services = builder.Services;
 
-app.Run();
+services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+
+services.AddAuthorization();
+
+var application = builder.Build();
+
+application.UseAuthentication();
+application.UseAuthorization();
+
+application.MapGet("/", () => "Hello from the Identity!");
+application.MapGet("/protected", () => "Secret!").RequireAuthorization();
+application.MapGet("/login", (HttpContext context) =>
+{
+    context.SignInAsync(new ClaimsPrincipal(new[]
+    {
+        new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString())
+            },
+            CookieAuthenticationDefaults.AuthenticationScheme
+        )
+    }));
+
+    return "Ok";
+});
+
+application.Run();
