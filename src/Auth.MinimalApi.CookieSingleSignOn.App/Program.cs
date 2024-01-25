15dd1095cd9132c9b45053ab510a87eb78836488
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-25 06:57:49 +0300

    Use Microsoft.AspNetCore.DataProtection.StackExchangeRedis

--- src/Auth.MinimalApi.CookieSingleSignOn.App/Program.cs
+++ src/Auth.MinimalApi.CookieSingleSignOn.App/Program.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
+
+services.AddDataProtection()
+    .PersistKeysToStackExchangeRedis(ConnectionMultiplexer.Connect("127.0.0.1"))
+    .SetApplicationName("unique");
 
 services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
--- src/Auth.MinimalApi.CookieSingleSignOn.Identity/Program.cs
+++ src/Auth.MinimalApi.CookieSingleSignOn.Identity/Program.cs
@@ -1,13 +1,21 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
+services.AddDataProtection()
+    .PersistKeysToStackExchangeRedis(ConnectionMultiplexer.Connect("127.0.0.1"))
+    .SetApplicationName("unique");
+
 services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+    .AddCookie(
+        CookieAuthenticationDefaults
+            .AuthenticationScheme /*, options => { options.Cookie.Domain = ".company.local";} */);
 
 services.AddAuthorization();
 
