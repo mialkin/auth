4cd44fbdc95d5f47bd08a3c2fd737c2be59e94c6
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-25 19:32:44 +0300

    Implement JWT authentication

--- src/Auth.Jwt.Server/Program.cs
+++ src/Auth.Jwt.Server/Program.cs
@@ -1,12 +1,77 @@
+using System.Security.Claims;
 using System.Security.Cryptography;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 var rsa = RSA.Create();
 rsa.ImportRSAPrivateKey(File.ReadAllBytes("key"), out _);
 
 var builder = WebApplication.CreateBuilder(args);
 
+var services = builder.Services;
+
+services.AddAuthentication()
+    .AddJwtBearer("jwt", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+            ValidateIssuer = false
+        };
+        
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                if (context.Request.Query.ContainsKey("t"))
+                {
+                    context.Token = context.Request.Query["t"];
+                }
+
+                return Task.CompletedTask;
+            }
+        };
+
+        options.Configuration = new OpenIdConnectConfiguration
+        {
+            SigningKeys =
+            {
+                new RsaSecurityKey(rsa)
+            }
+        };
+
+        options.MapInboundClaims = false;
+    });
+
 var application = builder.Build();
 
-application.MapGet("/", () => "Hello World!");
+application.UseAuthentication();
+
+application.MapGet("/", (HttpContext context) =>
+{
+    var claim = context.User.FindFirst("sub")?.Value ?? "Empty";
+
+    return claim;
+});
+application.MapGet("/jwt", () =>
+{
+    var handler = new JsonWebTokenHandler();
+    var key = new RsaSecurityKey(rsa);
+    var token = handler.CreateToken(new SecurityTokenDescriptor
+    {
+        Issuer = "http://localhost:5070",
+        Subject = new ClaimsIdentity(
+            new[]
+            {
+                new Claim("sub", Guid.NewGuid().ToString()),
+                new Claim("name", "aleksei")
+            }),
+        SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256)
+    });
+
+    return token;
+});
 
 application.Run();
