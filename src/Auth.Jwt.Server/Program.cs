2fdaec1fa084e471e089e5c93a71cac7bda5425e
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-25 18:14:07 +0300

    Generate and use private key

--- src/Auth.Jwt.Server/Program.cs
+++ src/Auth.Jwt.Server/Program.cs
@@ -1,3 +1,8 @@
+using System.Security.Cryptography;
+
+var rsa = RSA.Create();
+rsa.ImportRSAPrivateKey(File.ReadAllBytes("key"), out _);
+
 var builder = WebApplication.CreateBuilder(args);
 
 var application = builder.Build();
