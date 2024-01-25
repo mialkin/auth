4118eb461ebfd0a17e40e805339d9d850a72400e
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-25 16:47:45 +0300

    Add RawCoding Jwt projects

---
+++ src/Auth.Jwt.KeyGen/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Security.Cryptography;
+
+var rsa = RSA.Create();
+var privateKey = rsa.ExportRSAPrivateKey();
+
+File.WriteAllBytes("key", privateKey);
---
+++ src/Auth.Jwt.Server/Program.cs
@@ -0,0 +1,7 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var application = builder.Build();
+
+application.MapGet("/", () => "Hello World!");
+
+application.Run();
