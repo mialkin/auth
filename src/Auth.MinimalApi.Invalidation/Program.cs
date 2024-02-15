72d18524bcb8a5baa13e4e11a0ceaa459cd80d53
Aleksei Mialkin <aleksei@mialkin.ru>
2024-02-15 23:47:27 +0300

    Add initial setup

--- src/Auth.MinimalApi.Invalidation/Program.cs
+++ src/Auth.MinimalApi.Invalidation/Program.cs
@@ -1,7 +1,27 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication("cookie")
+    .AddCookie("cookie");
 
 var application = builder.Build();
 
-application.MapGet("/", () => "Hello World!");
+application.MapGet("/login", () => Results.SignIn(
+        new ClaimsPrincipal(
+            new ClaimsIdentity(
+                new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                },
+                authenticationType: "cookie"
+            )
+        ),
+        new AuthenticationProperties(),
+        "cookie"
+    )
+);
+
+application.MapGet("/user", (ClaimsPrincipal user) => user.Claims.Select(x => new { x.Type, x.Value }).ToList());
 
 application.Run();
