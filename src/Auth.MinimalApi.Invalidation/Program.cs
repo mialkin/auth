c09c6912dec123342173c0fbd124b28f147433ed
Aleksei Mialkin <aleksei@mialkin.ru>
2024-02-16 00:07:16 +0300

    Finish example

--- src/Auth.MinimalApi.Invalidation/Program.cs
+++ src/Auth.MinimalApi.Invalidation/Program.cs
@@ -1,9 +1,23 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 
+var blackList = new List<string>();
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthentication("cookie")
-    .AddCookie("cookie");
+    .AddCookie("cookie", options =>
+    {
+        options.Events.OnValidatePrincipal = context =>
+        {
+            // context.HttpContext.RequestServices.GetRequiredService<IBlackListStorage>();
+            if (blackList.Contains(context.Principal!.FindFirstValue("session")!))
+            {
+                context.RejectPrincipal();
+            }
+
+            return Task.CompletedTask;
+        };
+    });
 
 var application = builder.Build();
 
@@ -13,6 +27,7 @@ application.MapGet("/login", () => Results.SignIn(
                 new[]
                 {
                     new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                    new Claim("session", Guid.NewGuid().ToString()),
                 },
                 authenticationType: "cookie"
             )
@@ -23,5 +38,8 @@ application.MapGet("/login", () => Results.SignIn(
 );
 
 application.MapGet("/user", (ClaimsPrincipal user) => user.Claims.Select(x => new { x.Type, x.Value }).ToList());
+
+// http://localhost:5170/blacklist?session=33136d12-b6b8-4063-b7f7-071330d15f51
+application.MapGet("/blacklist", (string session) => { blackList.Add(session); });
 
 application.Run();
